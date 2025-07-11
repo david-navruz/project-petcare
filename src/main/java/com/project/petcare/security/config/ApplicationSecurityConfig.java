d9b86a6a5882c05bbe50a0b6b9abc94d229c4a16
David <david.navruz55@gmail.com>
2025-07-11 19:07:52 +0200

    Updating security components

--- src/main/java/com/project/petcare/security/config/ApplicationSecurityConfig.java
+++ src/main/java/com/project/petcare/security/config/ApplicationSecurityConfig.java
@@ -61,7 +61,9 @@ public class ApplicationSecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.csrf(AbstractHttpConfigurer::disable)
+        http
+                .cors(cors -> {})
+                .csrf(AbstractHttpConfigurer::disable)
                 .exceptionHandling(exception -> exception.authenticationEntryPoint(authEntryPoint))
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth.requestMatchers(SECURED_URLS.toArray(String[]::new)).authenticated()
